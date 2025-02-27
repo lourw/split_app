0685b35019cd68897394fd7a0a75ecc972eab509
lourw <56288712+lourw@users.noreply.github.com>
2025-02-26 23:51:57 -0800

    fix(users): make tests pass

--- test/split_app_web/controllers/page_controller_test.exs
+++ test/split_app_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SplitAppWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Track & Split Expenses Without The Drama"
   end
 end
